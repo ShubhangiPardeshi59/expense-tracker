cbc0f0fc84ad41066a83ac030e0229a0621b1f33
ShubhangiPardeshi59 <shubhangi.pardeshi@ellicium.com>
2023-10-16 13:25:43 +0530

    start to using MUI

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,16 +1,28 @@
-import {Link} from "react-router-dom";
+import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { lime, purple } from '@mui/material/colors';
+import Button from '@mui/material/Button';
 
-export default function Header() {
-  
+const theme = createTheme({
+  palette: {
+    primary: lime,
+    secondary: purple,
+  },
+});
+
+const style = {
+  backgroundColor : purple["500"]
+}
+
+export default function UsingColorObject() {
   return (
-    <div className="header" id="main-header">
-      <div className="header-tittle">
-        <h1>Personal Expense Tracker</h1>
-      </div>
-      <div className="header-navbar">
-        <Link to = "/events/new" className="button btn-1">Add Expense</Link>
-        <button className="button">All List</button>
-      </div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Button variant="contained">Primary</Button>
+      <Button variant="contained" color="secondary" sx={{ ml: 2 }}>
+        Secondary
+      </Button>
+      <Button sx={{backgroundColor : purple[700]}}>All Expense</Button>
+      <Button variant="contained" sx={style}>Primary</Button>
+    </ThemeProvider>
   );
 }
